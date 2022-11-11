55872653250924bd51c488118587e872369c0394
ootsumasaya <capibaratotoro@gmail.com>
2022-11-11 20:34:25 +0900

    popupの削除

--- popup.js
+++
@@ -1,6 +0,0 @@
-// devtools.jsで保存したanswersを読み込み
-answers = localStorage.getItem("answers");
-// popup.htmlのElementを取得
-let answersElem = document.getElementById('answers');
-// 取得したElementにanswersを埋め込む
-answersElem.innerHTML = answers;
